ac6eda7c51f9da2dc9162b2492d41984e9b926b9
Edgar Elizarraras <ed6eabc@gmail.com>
2023-08-02 17:53:26 -0600

    Changes:   - Moved Greeting function to LoginScreen.kt   - Created a function MySuperText and MyTestPreview to see how preview works.

---
+++ app/src/main/java/com/example/jetpackcompose/LoginScreen.kt
@@ -0,0 +1,13 @@
+package com.example.jetpackcompose
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Esto es un ejemplo",
+        modifier = modifier
+    )
+}
--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,18 +31,49 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeTheme {
         Greeting("Android")
     }
+}
+
+//Note: if our preview get parameter it won't work
+// To fix this we can use a wrapper, see line 60
+/*@Preview(
+    name = "Titulo de la preview",
+    heightDp = 50,
+    widthDp = 200,
+    showBackground = true,
+    showSystemUi = true,
+    device = "spec:width=411dp,height=891dp",
+    backgroundColor = 0xFFC44242,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    wallpaper = Wallpapers.NONE
+)
+@Composable
+fun MySuperText(mensajeExterno: String){
+    Text(text = "Mi texto random, $mensajeExterno")
+}*/
+
+@Composable
+fun MySuperText(mensajeExterno: String){
+    Text(text = "Mi texto random, $mensajeExterno")
+}
+
+@Preview(
+    name = "Titulo de la preview",
+    heightDp = 50,
+    widthDp = 200,
+    showBackground = true,
+    showSystemUi = true,
+    device = "spec:width=411dp,height=891dp",
+    backgroundColor = 0xFFC44242,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    wallpaper = Wallpapers.NONE
+)
+@Composable
+fun MyTestPreview(){
+    MySuperText("Mensaje externo")
 }
